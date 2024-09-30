e41e56c1ae4c93d36d1d4343d15966d5ca6ce239
miminmoons <jasmine.saphira@mail.utoronto.ca>
2024-09-30 17:59:18 -0400

    Finished

---
+++ week3git/src/main/java/org/translation/SortUtil.java
@@ -0,0 +1,21 @@
+package org.translation;
+
+import java.util.List;
+
+public class SortUtil {
+
+    public static void sort(List<String> list) {
+        // Simple bubble sort implementation
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+                    // Swap list[j] and list[j+1]
+                    String temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
+}
